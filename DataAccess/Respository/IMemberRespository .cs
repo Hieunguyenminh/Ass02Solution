cc982bdd84fa1f2e8d811db73ec33969088456dd
DESKTOP-M8FB9D3\Hieu Nguyen <hieunguyen3141592@gmail.com>
2022-06-30 15:14:39 +0700

    Added IMemberRespo and MemberRespo

--- DataAccess/Respository/IMemberRespository .cs
+++ DataAccess/Respository/IMemberRespository .cs
@@ -1,1 +1,16 @@
-﻿
+﻿using BussinessObject.Models;
+
+namespace DataAcess.Respository
+{
+    public interface IMemberRespository
+    {
+        IEnumerable<Member> GetMembers();
+        IEnumerable<Member> GetMembersByIDList(int memberID);
+        Member GetMemberByID(int memberID);
+        Member GetMemberByCompanyName(string companyName);
+        List<Member> GetMembersByCityAndCountry(string city, string country);
+        void InsertMember(Member member);
+        void UpdateMember(Member member);
+        void DeleteMember(int memberID);
+    }//IMemberRespository interface end
+}//DataAcess.Respository namespace end
